a4ddc00880db0491c699578f20ffc4611e1b7a3b
Ramprit Sahani <21260321+Ramprits@users.noreply.github.com>
2019-11-04 14:03:41 +0530

    adding container css for all pages

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import Navigation from "./components/Navigation";
 import { AuthProvider } from "./context/authContext";
 import AuthRoute from "./utils/authGaurd";
+import Navigation from "./components/Navigation";
+
 import Home from "./pages/home";
 import SignUp from "./pages/signup";
 import SignIn from "./pages/signin";
+
 const App = () => {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Navigation />
-        <div className="container" style={{ marginTop: "2em" }}>
+        <div className="container" style={{ marginTop: "3em" }}>
           <Switch>
             <Route path="/home" component={Home} />
             <AuthRoute path="/signup" component={SignUp} />
